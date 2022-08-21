79cf32cf30e6a2ecd9796ada629ebddca7747490
Nangupangu <109413698+Nangupangu@users.noreply.github.com>
2022-08-21 11:52:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import requests
 
 channelID = PUT THE CHANNEL ID
 headers = {
-    "authorization":
-    "YOUR TOKEN HERE"
+    "989512298068594748":
+    "OTkxNjA3Njk4OTQyNTk5MjYw.GojeAy.JyMrCDgSwHS8PHbuiNO9PS4A1cAKx8EOW7PQlg"
 }
 keep_alive()
 file = open("text.txt", "r")
